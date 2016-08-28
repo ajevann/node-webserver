f855febd0373dd7697535ee7f98fd300a0351f0b
Alex Vann <ajevann@gmail.com>
2016-08-28 00:41:12 -0400

    still debugging, added multiple server idea

--- server.js
+++ server.js
@@ -1,14 +1,73 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var webserver = {
+  hostname: '127.0.0.1',
+  port: 3000,
+  indexFiles: [],
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+  init: function() {
+    webserver.grabIndexes('.', function() {
+      console.log('printing... ', webserver.indexFiles.length);
+      for (var indexFile in webserver.indexFiles) {
+        webserver.port++;
+        console.log(webserver.port, webserver.indexFiles[indexFile]);
+        webserver.startServer(webserver.port, indexFile);
+      }
+    });
+  },
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  grabIndexes: function(dir, done) {
+    fs.readdir(dir, function(error, list) {
+      if (error) {
+        return done(error);
+      }
+
+      var i = 0;
+
+      (function next() {
+        var file = list[i++];
+
+        if (!file) {
+          return done(null);
+        }
+
+        var fileName = file;
+        var filePath = dir + '/' + file;
+        file = dir + '/' + file;
+
+        fs.stat(filePath, function(error, stat) {
+
+          if (stat && stat.isDirectory()) {
+            webserver.grabIndexes(filePath, function(error) {
+              next();
+            });
+          } else {
+            // do stuff to file here
+            if (fileName === 'index.html') {
+              webserver.indexFiles.push(filePath);
+
+              console.log(filePath);
+            }
+            next();
+          }
+        });
+      })();
+    });
+  },
+
+  startServer: function(port, path) {
+    console.log('>>>', port, path);
+    http.createServer(function(req, res) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      var index = fs.readFile(path);
+      res.end(index);
+    }).listen(port, function() {
+      console.log('Server running at http://' + webserver.hostname + ':' + port + '/');
+    });
+  }
+}
+
+webserver.init();
